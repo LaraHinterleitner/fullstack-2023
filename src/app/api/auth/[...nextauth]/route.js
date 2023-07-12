fb22b1052fb69f01c0450de4ee94634eda042464
Lara Hinterleitner <lhinterleitner.mmt-b2021@fh-salzburg.ac.at>
2023-07-12 17:04:15 +0200

    changed some things

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
+  // secret: process.env.NEXTAUTH_SECRET,
 
   providers: [
     GithubProvider({
@@ -11,7 +11,8 @@ export const authOptions = {
     }),
   ],
 
-  database: process.env.DATABASE_URL,
+  // adapter: PrismaAdapter(prisma),
+  // database: process.env.DATABASE_URL,
 }
 
 const handler = NextAuth(authOptions)

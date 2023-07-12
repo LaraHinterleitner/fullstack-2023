bcb185a5f8c94a34635e30c1a3a01c8f9767f9cd
Lara Hinterleitner <lhinterleitner.mmt-b2021@fh-salzburg.ac.at>
2023-07-12 17:08:47 +0200

    fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,23 +1,23 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+// import NextAuth from 'next-auth'
+// import GithubProvider from 'next-auth/providers/github'
 
-export const authOptions = {
-  // secret: process.env.NEXTAUTH_SECRET,
+// export const authOptions = {
+//   // secret: process.env.NEXTAUTH_SECRET,
 
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//     }),
+//   ],
 
-  // adapter: PrismaAdapter(prisma),
-  // database: process.env.DATABASE_URL,
-}
+//   // adapter: PrismaAdapter(prisma),
+//   // database: process.env.DATABASE_URL,
+// }
 
-const handler = NextAuth(authOptions)
+// const handler = NextAuth(authOptions)
 
-export {
-  handler as GET,
-  handler as POST
-}
+// export {
+//   handler as GET,
+//   handler as POST
+// }
